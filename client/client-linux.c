29dea0dfea290645150aed88253ff2c90fc02671
Daniel Paulo Garcia <38893804+daniel-pg@users.noreply.github.com>
2024-05-20 22:56:12 -0300

    refactor: _send_command_mainloop

--- client/client-linux.c
+++ client/client-linux.c
@@ -45,56 +45,17 @@ static void _get_server_addrinfo(
     }
 }
 
-int main(int argc, char *argv[]) {
-    char *srv_hostname, *srv_port;
-
-    // Get server's ip:port from the command line arguments.
-    if (argc > 3) {
-        fprintf(stderr, "Usage: client [HOSTNAME [PORT]]\n");
-        exit(EX_USAGE);
-    } else {
-        srv_hostname = (argc >= 2) ? argv[1] : NULL;
-        srv_port = (argc == 3) ? argv[2] : DEFAULT_PORT;
-    }
-
-    struct addrinfo *srv_info_tcp, *srv_info_udp;
-    _get_server_addrinfo(srv_hostname, srv_port, &srv_info_tcp, &srv_info_udp);
-
-    // Create TCP socket
-    struct addrinfo *p;
-    int sockfd_tcp, sockfd_udp;
-    for(p = srv_info_tcp; p != NULL; p = p->ai_next) {
-        if (-1 != (sockfd_tcp = socket(p->ai_family, p->ai_socktype, p->ai_protocol))) {
-            break;
-        }
-    }
-
-    if (sockfd_tcp == -1) {
-        perror("Failed to create TCP socket");
-        exit(1);
-    }
-
-    // Create UDP socket
-    for(p = srv_info_udp; p != NULL; p = p->ai_next) {
-        if (-1 != (sockfd_udp = socket(p->ai_family, p->ai_socktype, p->ai_protocol))) {
-            break;
-        }
-    }
-
-    if (sockfd_udp == -1) {
-        perror("Failed to create UDP socket");
-        exit(1);
-    }
+static void _send_command_mainloop(int sockfd_tcp, int sockfd_udp, struct addrinfo *p_tcp, struct addrinfo *p_udp) {
+    char message[1024];
+    char server_reply[2048];
 
     // Connect to TCP socket
-    if (-1 == connect(sockfd_tcp, p->ai_addr, p->ai_addrlen)) {
+    if (-1 == connect(sockfd_tcp, p_tcp->ai_addr, p_tcp->ai_addrlen)) {
         perror("Connection error");
         exit(1);
     }
     puts("Client connected succesfully!\n");
 
-    char message[1024];
-    char server_reply[2048];
     while (true) {
         printf("Enter message: ");
         if (NULL == fgets(message, sizeof(message), stdin)) {
@@ -128,6 +89,50 @@ int main(int argc, char *argv[]) {
             puts(server_reply);
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    char *srv_hostname, *srv_port;
+
+    // Get server's ip:port from the command line arguments.
+    if (argc > 3) {
+        fprintf(stderr, "Usage: client [HOSTNAME [PORT]]\n");
+        exit(EX_USAGE);
+    } else {
+        srv_hostname = (argc >= 2) ? argv[1] : NULL;
+        srv_port = (argc == 3) ? argv[2] : DEFAULT_PORT;
+    }
+
+    struct addrinfo *srv_info_tcp, *srv_info_udp;
+    _get_server_addrinfo(srv_hostname, srv_port, &srv_info_tcp, &srv_info_udp);
+
+    // Create TCP socket
+    struct addrinfo *p_tcp, *p_udp;
+    int sockfd_tcp, sockfd_udp;
+    for(p_tcp = srv_info_tcp; p_tcp != NULL; p_tcp = p_tcp->ai_next) {
+        if (-1 != (sockfd_tcp = socket(p_tcp->ai_family, p_tcp->ai_socktype, p_tcp->ai_protocol))) {
+            break;
+        }
+    }
+
+    if (sockfd_tcp == -1) {
+        perror("Failed to create TCP socket");
+        exit(1);
+    }
+
+    // Create UDP socket
+    for(p_udp = srv_info_udp; p_udp != NULL; p_udp = p_udp->ai_next) {
+        if (-1 != (sockfd_udp = socket(p_udp->ai_family, p_udp->ai_socktype, p_udp->ai_protocol))) {
+            break;
+        }
+    }
+
+    if (sockfd_udp == -1) {
+        perror("Failed to create UDP socket");
+        exit(1);
+    }
+
+    _send_command_mainloop(sockfd_tcp, sockfd_udp, p_tcp, p_udp);
 
     freeaddrinfo(srv_info_tcp);
     freeaddrinfo(srv_info_udp);
