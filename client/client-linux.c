a4ea94057081d7c08132ad9830107e05104d4b5c
Daniel Paulo Garcia <38893804+daniel-pg@users.noreply.github.com>
2024-05-20 21:40:35 -0300

    refactor: extrai função _get_server_addrinfo para legibilidade

--- client/client-linux.c
+++ client/client-linux.c
@@ -12,11 +12,42 @@
 
 #define DEFAULT_PORT "8080"
 
-int main(int argc, char* argv[]) {
-    struct addrinfo hints;
-    struct addrinfo *srv_info;
-    char* srv_hostname;
-    char* srv_port;
+static void _get_server_addrinfo(
+    const char *restrict srv_hostname,
+    const char *restrict srv_port,
+    struct addrinfo *srv_info_tcp,
+    struct addrinfo *srv_info_udp
+) {
+
+    struct addrinfo hints_tcp, hints_udp;
+
+    // Get list of network addresses for server
+    memset(&hints_tcp, 0, sizeof(hints_tcp));
+    memset(&hints_udp, 0, sizeof(hints_udp));
+
+    hints_tcp.ai_family = AF_UNSPEC;
+    hints_tcp.ai_socktype = SOCK_STREAM;
+    hints_tcp.ai_protocol = getprotobyname("tcp")->p_proto;
+
+    hints_udp.ai_family = AF_UNSPEC;
+    hints_udp.ai_socktype = SOCK_DGRAM;
+    hints_udp.ai_protocol = getprotobyname("udp")->p_proto;
+
+    int errcode;
+    if (0 != (errcode = getaddrinfo(srv_hostname, srv_port, &hints_tcp, &srv_info_tcp))) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(errcode));
+        exit(1);
+    }
+
+    if (0 != (errcode = getaddrinfo(srv_hostname, srv_port, &hints_udp, &srv_info_udp))) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(errcode));
+        exit(1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    struct addrinfo *srv_info_tcp, *srv_info_udp;
+    char *srv_hostname, *srv_port;
     int sockfd;
 
     // Get server's ip:port from the command line arguments.
@@ -28,20 +59,13 @@ int main(int argc, char* argv[]) {
         srv_port = (argc == 3) ? argv[2] : DEFAULT_PORT;
     }
 
-    // Get list of network addresses for server
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = getprotobyname("tcp")->p_proto;
+    _get_server_addrinfo(srv_hostname, srv_port, srv_info_tcp, srv_info_udp);
 
-    int errcode;
-    if (0 != (errcode = getaddrinfo(srv_hostname, srv_port, &hints, &srv_info))) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(errcode));
-        exit(1);
-    }
+    // TODO: Colocar dentro do loop e trocar conexão UDP/TCP conforme necessário
+    struct addrinfo *p, *srv_info;
+    bool use_udp_socket = false;
 
-    // TODO
-    struct addrinfo* p;
+    srv_info = (use_udp_socket) ? srv_info_udp : srv_info_tcp;
     for(p = srv_info; p != NULL; p = p->ai_next) {
         if (-1 != (sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))) {
             break;
@@ -58,6 +82,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     puts("Client connected succesfully!\n");
+    // TODO: END
 
     char message[1024];
     char server_reply[2048];
@@ -95,6 +120,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    freeaddrinfo(srv_info);
+    freeaddrinfo(srv_info_tcp);
+    freeaddrinfo(srv_info_udp);
     return 0;
 }
